93899a08bcc730ed33fc5287afe280918b158bcb
Mike Morton <mikmort@hotmail.com>
2025-07-15 18:13:05 +0200

    fix vite root

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: 'src',
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    // output to project level dist instead of src/dist
+    outDir: '../dist',
   },
 });
