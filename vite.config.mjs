967b05df31b8fe4d86b7f2dd1394f414c81bb04f
Mike Morton <mikmort@hotmail.com>
2025-07-15 23:59:31 +0200

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  root: path.resolve(__dirname, 'src'),
-  publicDir: path.resolve(__dirname, 'public'), // 👈 explicitly use root-level /public
   plugins: [react()],
+  publicDir: 'public',
   build: {
-    outDir: path.resolve(__dirname, 'dist'),
+    outDir: 'dist',
     emptyOutDir: true
   }
 })
