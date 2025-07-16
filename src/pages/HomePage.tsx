84d6c358fdc67942c2e7f9dd1a8c6fb70b67acad
Mike Morton <mikmort@hotmail.com>
2025-07-16 11:43:34 +0200

    Add splash screen design

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,27 @@
+import GettingStartImage from '../images/GettingStartImage.jpg';
+
 interface Props {
   next: () => void;
 }
 
 function HomePage({ next }: Props) {
   return (
-    <div>
-      <p>Welcome! This wizard will help you configure Dynamics 365 Business Central.</p>
-      <button onClick={next}>Start</button>
+    <div className="splash-screen">
+      <div className="splash-content">
+        <h1>Business Central Setup</h1>
+        <p>
+          Welcome! This wizard will help you configure Dynamics 365 Business
+          Central.
+        </p>
+        <button className="splash-button" onClick={next}>
+          Get Started
+        </button>
+      </div>
+      <img
+        className="splash-image"
+        src={GettingStartImage}
+        alt="Getting started"
+      />
     </div>
   );
 }
