0f6f81a0397bd923e12d90d3b6e721ac9e03b02a
Mike Morton <mikmort@hotmail.com>
2025-07-16 13:29:38 +0200

    Update openai.js

--- public/openai.js
+++ public/openai.js
@@ -1,6 +1,6 @@
 // Rename to openai.js and fill in your Azure OpenAI details
 window.azureOpenAIConfig = {
-  endpoint: 'https://mikmo-md5vcthi-eastus2.cognitiveservices.azure.com/',
-  apiKey: 'FB22Q3tHarghUxkfa8eQMcfYiRiv6CgSqoqowfNPPGGWwIRguG9YJQQJ99BGACYeBjFXJ3w3AAABACOGjnwx',
+  endpoint: 'https://mikmo-md5vcthi-eastus2.cognitiveservices.azure.com/openai/deployments/gpt-4o-mini/chat/completions?api-version=2025-01-01-preview',
+  apiKey: 'D7iJLX1BNeJDFu7cdSmkvpjSUTafwB11V21qFTbDg0q93w2gd1rSJQQJ99BGACHYHv6XJ3w3AAAAACOGTcsb',
   deployment: 'gpt-4o'
 };
